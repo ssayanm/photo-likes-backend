391f0e90c47f394775f638a654fbdd4a53f524fe
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-07-29 08:46:19 +0530

    uploading the backend files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c1874320ed80cdd6e9505ac1569b8467'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '14061a7a-ddff-4b56-98a1-ad1ae1e4ed4a'
+};
