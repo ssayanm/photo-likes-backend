cf7a57b98546cce612896f71d8ed386a03e18e37
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-07-30 11:32:23 +0530

    added cron and a new policy

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Cron config that gives you an opportunity
@@ -18,4 +18,31 @@ module.exports = {
   // '0 1 * * 1': () => {
   //
   // }
+
+  "0 0 0 * * *": async () => {
+    console.log("Once a day, at midnight");
+
+    //Retrieve the users
+    const users = await strapi.plugins[
+      "users-permissions"
+    ].services.user.fetchAll({});
+
+    const res = await Promise.all(
+      users.map(async (user) => {
+        //Retrieve all posts created by this user
+        const posts = await strapi.services.post.find({ author: user.id });
+
+        console.log("posts.length", posts.length);
+
+        const total = posts.reduce((acc, post) => acc + post.likes, 0);
+
+        await strapi.plugins["email"].services.email.send({
+          to: user.email,
+          from: "Strapitest@localhost.com",
+          subject: "Your likes total",
+          text: `You got ${total} likes!`,
+        });
+      })
+    );
+  },
 };
